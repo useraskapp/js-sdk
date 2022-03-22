c8e442c426acfe3917388ae32d9817a8183eb3cc
unknown <harish.deiva@gmail.com>
2022-03-22 11:37:58 +0530

    fix-webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    entry: {
+        'index': './src/index.ts',
+    },
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ["style-loader", "css-loader",
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require("sass"),
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    devtool: "source-map",
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: "./dist/[name].js",
+        libraryTarget: "umd",
+        library: "js-sdk"
+    },
+};
